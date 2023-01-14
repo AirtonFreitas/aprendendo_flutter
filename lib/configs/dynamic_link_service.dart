d22f23719a615aa3464fd663ff318612b6ac62e2
AirtoFreitas <airtonsiqueiradefreitas@gmail.com>
2023-01-14 02:10:18 -0300

    inclui dynamic link

--- lib/configs/dynamic_link_service.dart
+++ lib/configs/dynamic_link_service.dart
@@ -1,27 +1,27 @@
-import 'package:aprendendo_flutter/features/widgets/widgets_page.dart';
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
-import 'package:flutter/material.dart';
-
-class DynamicLinkService {
-  final FirebaseDynamicLinks _firebaseDynamicLinks = FirebaseDynamicLinks();
-
-  Future<void> retrieveDynamicLink(BuildContext context) async {
-    try {
-      final PendingDynamicLinkData? data =
-          await FirebaseDynamicLinks.instance.getInitialLink();
-
-      final link = await _firebaseDL
-
-      if (deepLink != null) {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => WidgetsPage()));
-      }
-
-      FirebaseDynamicLinks.instance.onLink;
-    } catch (e) {
-      print(e.toString());
-    }
-  }
-
-  ///createDynamicLink()
-}
+// import 'package:aprendendo_flutter/features/widgets/widgets_page.dart';
+// import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+// import 'package:flutter/material.dart';
+//
+// class DynamicLinkService {
+//   final FirebaseDynamicLinks _firebaseDynamicLinks = FirebaseDynamicLinks();
+//
+//   Future<void> retrieveDynamicLink(BuildContext context) async {
+//     try {
+//       final PendingDynamicLinkData? data =
+//           await FirebaseDynamicLinks.instance.getInitialLink();
+//
+//       final link = await _firebaseDL
+//
+//       if (deepLink != null) {
+//         Navigator.of(context)
+//             .push(MaterialPageRoute(builder: (context) => WidgetsPage()));
+//       }
+//
+//       FirebaseDynamicLinks.instance.onLink;
+//     } catch (e) {
+//       print(e.toString());
+//     }
+//   }
+//
+//   ///createDynamicLink()
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'configs/utils/app_routes.dart';
 import 'configs/utils/colors_utils.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() {
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+
+    // Get any initial links
+    final PendingDynamicLinkData? initialLink =
+        await FirebaseDynamicLinks.instance.getInitialLink();
+
+    print('aaa');
+    print(initialLink.toString());
+  } catch (e) {
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,8 +37,11 @@ class MyApp extends StatelessWidget {
             buttonColor: ColorsUtil.black,
             textTheme: ButtonTextTheme.accent,
           ),
-          textTheme: ThemeData.light().textTheme.copyWith(
-              headline6: const TextStyle(fontFamily: 'Heavitas')), colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green).copyWith(secondary: ColorsUtil.black)),
+          textTheme: ThemeData.light()
+              .textTheme
+              .copyWith(headline6: const TextStyle(fontFamily: 'Heavitas')),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green)
+              .copyWith(secondary: ColorsUtil.black)),
       initialRoute: 'splash',
       onGenerateRoute: RouteGenerator.generateRoute,
       debugShowCheckedModeBanner: false,
